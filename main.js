b6bd9a167cd400f7837b9a71e82926bde9c6b305
Brooks West <brooksmwest@hotmail.com>
2024-04-22 12:22:21 -0500

    updated broken pet links for pictures

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const pets = [
       color: "Brown",
       specialSkill: "Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.",
       type: "dino",
-      imageUrl: "http://www.jozilife.co.za/wp-content/uploads/The-Dino-Expo.jpg",
+      imageUrl: "https://i.insider.com/5f08b7e5f34d053f663fbada?width=700",
     },
     {
       id: 3,
@@ -61,7 +61,7 @@ const pets = [
       color: "Grey",
       specialSkill: "Comfortable in the outdoors for up to eight hours.",
       type: "dino",
-      imageUrl: "http://www.theouthousers.com/images/jck//ThanosCopter/news/grumpasaur.jpg"
+      imageUrl: "https://th-thumbnailer.cdn-si-edu.com/FGt4QcvnVNNTnbLWawybTFljFO8=/fit-in/1072x0/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/2f/31/2f31b503-2ac2-4eae-b450-d6dda531a0bf/victorville-bad-dinosaur.jpg"
     },
     {
       id: 9,
@@ -85,7 +85,7 @@ const pets = [
       color: "Orange",
       specialSkill: "Can be around food without staring creepily at it.",
       type: "dino",
-      imageUrl: "http://www.cantref.com/userfiles/events/walking%20dino.jpg?w=600"
+      imageUrl: "https://as2.ftcdn.net/v2/jpg/05/80/20/29/1000_F_580202966_LVkbNqjjTlpeye7D2zOgaa4GrKM3BlsR.jpg"
     },
     {
       id: 12,
@@ -101,7 +101,7 @@ const pets = [
       color: "Red",
       specialSkill: "Expertly quotes and recognizes dialogue from early seasons of The Simpsons.",
       type: "dog",
-      imageUrl: "http://1kjkdg1axrkd2g03cnboj761.wpengine.netdna-cdn.com/wp-content/uploads/2017/12/braydon-anderson-105552-e1512684107659.jpg"
+      imageUrl: "https://static01.nyt.com/images/2013/12/02/arts/arts2/arts2-articleLarge.jpg?quality=75&auto=webp&disable=upscale"
     },
     {
       id: 14,
@@ -125,7 +125,7 @@ const pets = [
       color: "Brown",
       specialSkill: "Drives at a safe rate of speed in snow or rain.",
       type: "dino",
-      imageUrl: "https://images.readwrite.com/wp-content/uploads/2018/03/t-rex-dino-quiz-e1490854556549.jpg"
+      imageUrl: "https://as1.ftcdn.net/v2/jpg/05/70/85/58/1000_F_570855802_xZj0qCu0MbzpxS1OH833D1raYeynYqV0.jpg"
     },
     {
      id: 17,
@@ -133,7 +133,7 @@ const pets = [
       color: "Yellow",
       specialSkill: "Does not freak out if you havent seen his favorite movie (The Big Lebowski).",
       type: "cat",
-      imageUrl: "http://www.funnycatsite.com/pictures/Close_Up_Yawn.jpg"
+      imageUrl: "https://images.uncyclomedia.co/uncyclopedia/en/8/8b/Stupid_cat_3.jpg"
     },
     {
       id: 18,
@@ -141,7 +141,7 @@ const pets = [
       color: "Brown",
       specialSkill: "Proficient in air guitar",
       type: "dino",
-      imageUrl: "https://www.nation.co.ke/image/view/-/4078922/highRes/1742693/-/maxw/600/-/1453yvh/-/DINO.jpg"
+      imageUrl: "https://as1.ftcdn.net/v2/jpg/05/76/16/78/1000_F_576167835_saG1ZAWP9Mj1E1v1GNpf3oi3YyM6XeSq.jpg"
     },
     {
       id: 19,
@@ -149,7 +149,7 @@ const pets = [
       color: "Blue",
       specialSkill: "Listens attentively to boring stories.",
       type: "dog",
-      imageUrl: "http://dailynewsdig.com/wp-content/uploads/2014/03/Creative-And-Funny-Dog-Stock-Photography-Pictures-2.jpg"
+      imageUrl: "https://pics.craiyon.com/2023-05-25/ff9b0ee8421b4d60ad3c2ec5109bcdc1.webp"
     },
     {
       id: 20,
@@ -157,7 +157,7 @@ const pets = [
       color: "Black",
       specialSkill: "Uses litter box at appropriate hours.",
       type: "cat",
-      imageUrl: "http://www.funnycatsite.com/pictures/Lazy_White_Cat.jpg"
+      imageUrl: "https://i.redd.it/i-asked-chatgpt-to-make-a-cute-dog-and-then-make-him-even-v0-x8zgj23x6m2c1.jpg?width=768&format=pjpg&auto=webp&s=9de2909fd435040a670215885e40d6dbe07ef6ec"
     },
     {
       id: 21,
@@ -165,7 +165,7 @@ const pets = [
       color: "Red",
       specialSkill: "Owns a Nintendo Power Glove.",
       type: "dino",
-      imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715"
+      imageUrl: "https://i1.sndcdn.com/artworks-000298749006-5gqxt7-t500x500.jpg"
     },
     {
         id: 22,
@@ -173,7 +173,7 @@ const pets = [
       color: "Orange",
       specialSkill: "Is comfortable with jokes about his receding hairline.",
       type: "cat",
-      imageUrl: "http://funnyanimalphoto.com/wp-content/uploads/2013/08/cat_caught_mouse_thegatewaypundit.jpg"
+      imageUrl: "https://static.boredpanda.com/blog/wp-content/uploads/2018/04/funny-idiot-cats-45-5ae6ea7ce1a15__605.jpg"
     },
     {
         id: 23,
@@ -189,7 +189,7 @@ const pets = [
       color: "Brown",
       specialSkill: "Participates in karaoke but does not force others to go out to karaoke.",
       type: "dog",
-      imageUrl: "http://www.dogbreedplus.com/dog_breeds/images/basset-hound-4.jpg"
+      imageUrl: "https://hips.hearstapps.com/hmg-prod/images/lifestyle-bestdogbreeds-1577128927.png?crop=0.503xw:1.00xh;0.263xw,0&resize=1200:*"
     },
     {
       id: 25,
@@ -197,7 +197,7 @@ const pets = [
       color: "Red",
       specialSkill: "Knows the words to 4 rap songs.",
       type: "cat",
-      imageUrl: "http://funbk.s3.amazonaws.com/wp-content/uploads/2016/06/funny-cat-video-which-will-make-you-laugh-louder.jpg"
+      imageUrl: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/41a24659-0e76-4624-a79f-5cdf46790473/dgbamlx-47b88e05-b9c8-4592-b7a6-661d6aabb9dc.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQxYTI0NjU5LTBlNzYtNDYyNC1hNzlmLTVjZGY0Njc5MDQ3M1wvZGdiYW1seC00N2I4OGUwNS1iOWM4LTQ1OTItYjdhNi02NjFkNmFhYmI5ZGMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.JexfphXytAkL4GWt42QlfCMZtXNoqrCSRKwNEmelh40"
     },
     {
         id: 26,
@@ -237,7 +237,7 @@ const pets = [
       color: "Red",
       specialSkill: "Doesn’t get weirded out by the word “moist.”",
       type: "dino",
-      imageUrl: "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119"
+      imageUrl: "https://qph.cf2.quoracdn.net/main-qimg-4e3dcae8b85a830828533d31a795b868-lq"
     }
   ];
 
